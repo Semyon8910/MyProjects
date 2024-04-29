fd3998058a3a0c86665c2f96a39c9445a49a487d
Semyon <khram89@gmail.com>
2024-04-29 20:34:19 +0300

    projects

--- react/react-project/src/index.tsx
+++ react/react-project/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MainLayout from './Components/layout/MainLayout/MainLayout';
 import { BrowserRouter } from 'react-router-dom';
